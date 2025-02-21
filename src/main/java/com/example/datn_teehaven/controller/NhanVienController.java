7a637507d4ec60de5333f82a639ff50957500df5
Longduong01 <ilivedubai247@gmail.com>
2025-02-21 15:40:01 +0700

    21/02

--- src/main/java/com/example/datn_teehaven/controller/NhanVienController.java
+++ src/main/java/com/example/datn_teehaven/controller/NhanVienController.java
@@ -6,7 +6,7 @@ import com.example.datn_teehaven.service.NhanVienService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -152,5 +152,5 @@ public class NhanVienController {
             return "redirect:/admin/nhan-vien";
         }
     }
-    
+
 }
