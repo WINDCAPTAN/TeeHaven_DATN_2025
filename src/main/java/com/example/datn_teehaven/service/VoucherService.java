fcaa0d8e7f0d0aefb544f03d1a7644b074c03d9f
WINDCAPTAN <phongnhp23@gmail.com>
2025-03-06 22:01:45 +0700

    06/03

---
+++ src/main/java/com/example/datn_teehaven/service/VoucherService.java
@@ -0,0 +1,48 @@
+package com.example.datn_teehaven.service;
+
+
+
+import com.example.datn_teehaven.entyti.Voucher;
+
+import java.util.List;
+
+public interface VoucherService {
+
+    Voucher fillByMaVoucher();
+
+    List<Voucher> findAll();
+
+    Voucher findById(Long id);
+
+    void deleteById(Long id);
+
+    List<Voucher> fillAllDangDienRa();
+
+    List<Voucher> fillAll();
+
+
+
+    List<Voucher> fillAllDaKetThuc();
+
+    List<Voucher> fillAllSapDienRa();
+
+    Voucher save(Voucher voucher);
+
+    boolean checkMaTrung(String ma);
+
+
+
+    boolean checkTenTrung(String ten);
+
+    boolean checkTenTrungSua(String ma, String ten);
+
+    boolean checkName(Long id, String ten);
+
+    boolean checkCode(Long id, String ma);
+
+    Voucher update(Voucher voucher);
+
+    Voucher getById(Long id);
+
+    public void updateVoucherStatus();
+}
